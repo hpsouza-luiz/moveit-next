68357bc5c0f4a8757fc95806df57377eaaf64e86
hpsouza-luiz <hpsouza.luiz@gmail.com>
2021-02-25 21:18:44 -0300

    First upload

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import '../styles/global.css';
+
+import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext';
+import { useState } from 'react';
+import { CountdownProvider } from '../contexts/CountdownContext';
+
+function MyApp({ Component, pageProps }) {
+
+
+  return (
+    <ChallengesProvider>
+        <Component {...pageProps} />
+    </ChallengesProvider>
+  )
+}
+
+export default MyApp
